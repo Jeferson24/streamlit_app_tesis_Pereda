80255769677dc208e303b66126201600628e498a
Jeferson24 <66497922+Jeferson24@users.noreply.github.com>
2024-11-18 22:20:30 +0000

    Update streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ with st.sidebar:
   e_cs_mm= st.slider('Thickness of Interior Steel Layers (mm)', 2.00, 4.00, 3.04,0.01) #9
   Fy_ac_mpa=st.slider('Yield Strees Steel (Mpa)', 165, 345, 250,1) #10
   E_cau_mpa= st.slider('Vertical Elastic Modulus (Mpa)', 1.00, 2.00, 1.44,0.01) #11
-  G_cau_mpa = st.slider('Shear Modulus of Rubber (Mpa)', 0.3, 0.5, 0.392,0.001) #12
-  Fycort_pb_mpa = st.slider('Yield Shear Stress of Lead (Mpa)', 5, 8.33, 10,0.01) #13
+  G_cau_mpa = st.slider('Shear Modulus of Rubber (Mpa)', 0.300, 0.500, 0.392,0.001) #12
+  Fycort_pb_mpa = st.slider('Yield Shear Stress of Lead (Mpa)', 5.00, 8.33, 10,0.01) #13
 
   #'Di (mm)','Ht (mm)','Dl (mm)','W (kg)','e_pc (mm)','#cc','e_cc (mm)'
   #'#cs','e_cs (mm)','Fy_ac (MPa)','E_cau (MPa)','G_cau (MPa)','Fycort_pb (MPa)'
