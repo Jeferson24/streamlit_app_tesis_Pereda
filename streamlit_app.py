b20d9a661feb43d18a893b9530d3070389e5acc0
Jeferson24 <66497922+Jeferson24@users.noreply.github.com>
2024-11-18 14:42:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,24 @@ with st.expander('Mechanical Propierties of LRB Materials'):
 
 # Input features
 with st.sidebar:
-  st.header('Input features')
-  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male', 'female'))
-  
+  st.header('Input LRB propierties (Geometrical and mechanical')
+  #Input propierties
+  Di_mm = st.slider('LRB Diameter (mm)', 749.3, 952.5, 850.9,0.01) #1
+  Ht_mm = st.slider('LRB High (mm)', 407.64, 420.34, 410.00,0.01) #2
+  Dl_mm = st.slider('Lead Core Diameter (mm)', 114.3, 203.2, 165.1,0.01) #3
+  W_kg = st.slider('LRB Weight (kg)', 808, 1456, 1068,1) #4
+  e_pc_mm = st.slider('Thickness of Exterior Rubber Layer (mm)', 18, 20, 19,1) #5
+  cc_und = st.slider('Number Rubber Layers', 10, 30, 28,1) #6
+  e_cc_mm = st.slider('Thickness of Interior Rubber Layers (mm)', 5, 8, 10,1) #7
+  cs_und = st.slider('Number Steel Layers', 10, 30, 27,1) #8
+  e_cs_mm= st.slider('Thickness of Interior Steel Layers (mm)', 2, 4, 3.04,0.01) #9
+  Fy_ac_mpa=st.slider('Yield Strees Steel (Mpa)', 165, 345, 250,1) #10
+  E_cau_mpa= st.slider('Vertical Elastic Modulus (Mpa)', 1.00, 2.00, 1.44,0.01) #11
+  G_cau_mpa = st.slider('Shear Modulus of Rubber (Mpa)', 0.3, 0.5, 0.392,0.001) #12
+  Fycort_pb_mpa = st.slider('Yield Shear Stress of Lead (Mpa)', 5, 8.33, 10,0.01) #13
+
+  #'Di (mm)','Ht (mm)','Dl (mm)','W (kg)','e_pc (mm)','#cc','e_cc (mm)'
+  #'#cs','e_cs (mm)','Fy_ac (MPa)','E_cau (MPa)','G_cau (MPa)','Fycort_pb (MPa)'
   # Create a DataFrame for the input features
   data = {'island': island,
           'bill_length_mm': bill_length_mm,
